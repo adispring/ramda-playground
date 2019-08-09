3b05a2c6a09d373343bb1203e4fb0a5c0193340d
adispring <sunnyadi@163.com>
2019-08-09 15:38:56 +0800

    add text

--- src/r/formatConversation.js
+++ src/r/formatConversation.js
@@ -5,14 +5,26 @@ import { HighlightType } from '../constants/biz/proto'
 import { SpanType } from '../constants/biz/conversation'
 const { TITLE, SUB_TITLE } = HighlightType
 
-const normalSpanCreator = (offset, length) => ({
+const normalSpanCreator = (offset, length, txt) => ({
   offset,
   length,
-  spanType: SpanType.NORMAL
+  spanType: SpanType.NORMAL,
+  text: txt.substring(offset, offset + length)
 })
 
-const transformToSpans = (highlights, text) => {
-  const highlightsWithType = R.map(R.assoc('spanType', SpanType.HIGHLIGHT), highlights)
+const transformToSpans = (highlights, txt) => {
+  console.log(txt)
+  const highlightsWithType = R.map(
+    highlight =>
+      R.mergeRight(highlight, {
+        spanType: SpanType.HIGHLIGHT,
+        text: txt.substring(
+          highlight.offset,
+          highlight.offset + highlight.length
+        )
+      }),
+    highlights
+  )
   let spans = []
   if (highlightsWithType.length === 1) {
     spans = highlightsWithType
@@ -24,7 +36,11 @@ const transformToSpans = (highlights, text) => {
         const lastEnd = last.offset + last.length - 1
         const hasNormalSpan = lastEnd + 1 < item.offset
         if (hasNormalSpan) {
-          const span = normalSpanCreator(lastEnd + 1, item.offset - lastEnd - 1)
+          const span = normalSpanCreator(
+            lastEnd + 1,
+            item.offset - lastEnd - 1,
+            txt
+          )
           return R.concat(acc, [span, item])
         } else {
           return R.concat(acc, [item])
@@ -37,12 +53,12 @@ const transformToSpans = (highlights, text) => {
   const head = R.head(spans)
   const last = R.last(spans)
   if (head.offset > 0) {
-    const span = normalSpanCreator(0, head.offset)
+    const span = normalSpanCreator(0, head.offset, txt)
     spans.unshift(span)
   }
   const lastEndPlus1 = last.offset + last.length
-  if (lastEndPlus1 < text.length) {
-    const span = normalSpanCreator(lastEndPlus1, text.length - lastEndPlus1)
+  if (lastEndPlus1 < txt.length) {
+    const span = normalSpanCreator(lastEndPlus1, txt.length - lastEndPlus1, txt)
     spans.push(span)
   }
 
@@ -55,20 +71,21 @@ const formatSearchItem = ({ highlights, title, subtitle }) => {
   const type = R.pathOr(TITLE, [0, 'type'], highlights)
   const text = type === TITLE ? title : subtitle
   const spans = transformToSpans(highlights, text)
-  return ({
+  return {
     type,
     spans,
     highlights,
     title,
     subtitle
-  })
+  }
 }
 
-const transformedSpans = transformToSpans(searchItem.highlights, searchItem.title)
+const transformedSpans = transformToSpans(
+  searchItem.highlights,
+  searchItem.title
+)
 
 const formatedItem = formatSearchItem(searchItem)
 const formatedSubItem = formatSearchItem(searchItemSub)
 
 console.log(formatedItem)
-
-
